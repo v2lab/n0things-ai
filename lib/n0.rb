7717d7c798242b20ab1bc704f75ab3223409403d
Artem Baguinski <artm@v2.nl>
2012-08-30 14:00:14 +0200

    parse shapes from SimpleDB

--- lib/n0.rb
+++ lib/n0.rb
@@ -4,16 +4,17 @@ require 'JSON'
 require 'aws-sdk'
 
 module N0
-  CONFIG_PATH = 'data/n0-aws-credentials.json'
+  CONFIG_PATH = 'data/n0-config.json'
+
+  # load config...
   File.open(CONFIG_PATH, 'r') do |f|
     CONFIG = JSON.load f.readlines.join
   end
+
+  def N0.db(admin=false)
+    creds = CONFIG[if admin then 'simpledb_admin' else 'simpledb_access' end]
+    AWS.config(:access_key_id => creds['access_key_id'],
+               :secret_access_key => creds['secret_access_key'])
+    AWS::SimpleDB.new
+  end
 end
-
-creds = N0::CONFIG['simpledb_access']
-puts creds.inspect
-
-AWS.config(:access_key_id => creds['access_key_id'],
-           :secret_access_key => creds['secret_access_key'])
-
-# load n0things classes if any...
